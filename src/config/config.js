800bf1f67309cf9db8615761015d5d8c21c8d0e0
D'angelo Nakamura <dangelonakamura@gmail.com>
2024-09-05 17:01:54 -0300

    Add configuration file

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    episodio: "https://animesonlinecc.to/episodio/",
+    search: "https://animesonlinecc.to/search/",
+    animeTypes: "https://animesonlinecc.to/genero/",
+    anime:"https://animesonlinecc.to/anime/"
+}
